607a00feecf1c27d6bb80cf0e715b3d4155135e3
lucaslucenak <lucas.lucenak@gmail.com>
2022-03-28 21:44:03 -0300

    updated login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Formik, ErrorMessage, Field } from 'formik';
-import * as Yup from 'yup';
-import { authContext } from '../components/AuthProvider';
-import { useLocation, useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import { Formik, ErrorMessage, Field } from "formik";
+import * as Yup from "yup";
+import { authContext } from "../components/AuthProvider";
+import { useLocation, useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import styles from './Login/styles.css';
 
 const LoginSchema = Yup.object().shape({
-  username: Yup.string().required('Required'),
-  password: Yup.string().required('Required')
+  username: Yup.string().required("Required"),
+  password: Yup.string().required("Required"),
 });
 
 const LoginPage = () => {
@@ -19,9 +20,10 @@ const LoginPage = () => {
 
   const handleSubmitting = (values, { setSubmitting, setStatus }) => {
     setStatus({ isValidating: true });
-    login().then(navigate(from, { replace: true }))
-      axios.post('https://fast-badlands-00990.herokuapp.com/api/v1/login', values)
-        .then(resp => console.log(resp))
+    login().then(navigate(from, { replace: true }));
+    axios
+      .post("https://fast-badlands-00990.herokuapp.com/api/v1/login", values)
+      .then((resp) => console.log(resp));
     // setTimeout(() => {
     //   // let email = values.email
     //   // let senha = values.senha
@@ -33,40 +35,91 @@ const LoginPage = () => {
   };
 
   return (
-    <Formik
-      validationSchema={LoginSchema}
-      initialValues={{ username: '', password: ''}}
-      onSubmit={handleSubmitting}
-    >
-      {({
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Username*:
-            <Field type="text" name="username"
-                   onBlur={handleBlur}
-                   onChange={handleChange}/>
-          </label>
-          <ErrorMessage name="username" className="error" component="span"/>
-          <br/><br/>
-          <label>
-            Password*:
-            <Field type="password" name="password"
-                   onBlur={handleBlur}
-                   onChange={handleChange}/>
-          </label>
-          <ErrorMessage name="password" className="error" component="span" />
-          <br/><br/>
-          <input type="submit" value="Login" disabled={isSubmitting}/>
-          <Link to="/register">Register</Link>
-        </form>
-      )}
-    </Formik>
-  )
+    <div className="container">
+      <div class="d-flex justify-content-center h-100">
+        <div class="card">
+          <div class="card-header">
+            <h3>Sign In</h3>
+          </div>
+          <Formik
+            validationSchema={LoginSchema}
+            initialValues={{ username: "", password: "" }}
+            onSubmit={handleSubmitting}
+          >
+            {({ handleChange, handleBlur, handleSubmit, isSubmitting }) => (
+
+              <div class="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div class="input-group form-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="fas fa-user"></i>
+                      </span>
+                    </div>
+
+                    <Field
+                      class="form-control"
+                      placeholder="username"
+                      type="text"
+                      name="username"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                    />
+
+                    <ErrorMessage
+                      name="username"
+                      className="error"
+                      component="span"
+                    />
+                  </div>
+
+                  <div class="input-group form-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="fas fa-key"></i>
+                      </span>
+                    </div>
+
+                    <Field
+                      class="form-control"
+                      placeholder="password"
+                      type="password"
+                      name="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                    />
+
+                    <ErrorMessage
+                      name="password"
+                      className="error"
+                      component="span"
+                    />
+                  </div>
+
+                  <div class="form-group">
+                    <input
+                      type="submit"
+                      class="btn float-right login_btn"
+                      value="Login"
+                      disabled={isSubmitting}
+                    />
+                  </div>
+
+                </form>
+                </div>
+            )}
+          </Formik>
+          <div class="card-footer">
+            <div class="d-flex justify-content-center links">
+              Don't have an account?<Link to="/register">Sign up</Link>
+            </div>
+            
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
