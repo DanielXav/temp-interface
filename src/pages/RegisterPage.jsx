e4f50bd69a033f9c20d16bc969bdce8ca7dfe05d
Daniel Xavier <dxavier194@gmail.com>
2022-03-29 11:34:10 -0300

    RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,103 +1,122 @@
-import axios from "axios";
+import React from "react";
 import { Formik, ErrorMessage, Field } from "formik";
 import * as Yup from "yup";
-import { Link, useNavigate } from 'react-router-dom';
+import { authContext } from "../components/AuthProvider";
+import { useLocation, useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import SelectInput from "../components/SelectInput";
 
 const RegisterSchema = Yup.object().shape({
-    username: Yup.string().required("Informe seu username!"),
-    password: Yup.string().required("Informe sua senha!"),
-    matricula: Yup.string().required("Informe sua matricula!"),
-    papel: Yup.string().required("Informe seu papel!"),
-    funcao: Yup.string().required("Informe sua funcao!"),
+    username: Yup.string().required('Required'),
+    password: Yup.string().required('Required').min(4, 'Too short!').max(10, 'Too long!'),
+    registration: Yup.string().required("Informe sua registration!"),
+    role: Yup.string().required("Informe seu role!"),
+    function: Yup.string().required("Informe sua function!")
 });
 
 const RegisterPage = () => {
+    //   const { login } = React.useContext(authContext);
+    //   let navigate = useNavigate();
+    //   let location = useLocation();
 
-    const navigate = useNavigate();
+    //   let from = location.state?.from || "/";
 
     const handleSubmitting = (values, { setSubmitting, setStatus }) => {
         setStatus({ isValidating: true });
-        // login().then(navigate(from, { replace: true }))
-          axios.post('https://fast-badlands-00990.herokuapp.com/api/v1/login', values)
+        axios.post('https://fast-badlands-00990.herokuapp.com/api/v1/signup', values)
             .then(resp => console.log(resp))
+        // login().then(navigate(from, { replace: true }))
         // setTimeout(() => {
-        //   // let email = values.email
-        //   // let senha = values.senha
-    
-        //   console.info(JSON.stringify(values, null, 2));
-        //   setSubmitting(false); // iremos fazer modificações aqui
-        //   setStatus({ isValidating: false });
+        //     console.info(JSON.stringify(values, null, 2));
+        //     setSubmitting(false); // iremos fazer modificações aqui
+        //     setStatus({ isValidating: false });
         // }, 400);
+        // alert(JSON.stringify(values));
+    };
+
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        setTimeout(() => {
+            navigate("/login");
+        }, 400);
+       
       };
 
-      const home = () => {
-        navigate("/");
-      };
+    const optionsRoles = [
+        { value: "ALUMN", label: 'ALUMN' },
+        { value: "TEACHER", label: 'TEACHER' }
+    ]
 
-      return (
+    const optionsFunctions = [
+        { value: "TRAINEE", label: 'TRAINEE' },
+        { value: "JUNIOR", label: 'JUNIOR' },
+        { value: "SENIOR", label: 'SENIOR' },
+        { value: "MASTER", label: 'MASTER' },
+        { value: "COORDINATOR", label: 'COORDINATOR' }
+    ]
+
+    return (
         <Formik
-        validationSchema={RegisterSchema}
-        initialValues={{
-            username: "",
-            password: "",
-            matricula: "",
-            papel: "",
-            funcao: ""
-        }}
-        onSubmit={handleSubmitting}
-    >
-        {({ handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-            <div className="container">
-                <div className="div-class-conteiner">
-                    <div className="text-center mt-5">
-                        <form onSubmit={handleSubmit} />
-                        <img className="mt-4" src="./img/logo.svg" alt="" height="72" />
-                        <h1 className="h3 mb-3 font-weight-normal">Cadastro</h1>
+            validationSchema={RegisterSchema}
+            initialValues={{ username: '', password: '', registration: '', role: '', function: '' }}
+            onSubmit={handleSubmitting}
+        >
+            {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Username*:
+                        <Field type="text" name="username"
+                            onBlur={handleBlur}
+                            onChange={handleChange} />
+                    </label>
+                    <ErrorMessage name="username" className="error" component="span" />
+                    <br /><br />
+                    <label>
+                        Password*:
+                        <Field type="password" name="password"
+                            onBlur={handleBlur}
+                            onChange={handleChange} />
+                    </label>
+                    <ErrorMessage name="password" className="error" component="span" />
+                    <br /><br />
+                    <label>
+                        Registration*:
+                        <Field type="number" name="registration"
+                            onBlur={handleBlur}
+                            onChange={handleChange} />
+                    </label>
+                    <ErrorMessage name="registration" className="error" component="span" />
+                    <br /><br />
+                    <SelectInput name="role">
+                        {optionsRoles.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </SelectInput>
+                    <ErrorMessage name="role" className="error" component="span" />
+                    <br /><br />
+                    <SelectInput name="function">
+                        {optionsFunctions.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </SelectInput>
+                    <ErrorMessage name="function" className="error" component="span" />
 
-                        <div>
-                            <input type="text" placeholder="Usuário" name="username" className="form-control mt-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" onBlur={handleBlur} onChange={handleChange} />
-                        </div>
-                        <ErrorMessage name="username" className="error" component="span" />
-
-                        <div>
-                            <input type="password" placeholder="Senha" name="password" className="form-control mt-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" onBlur={handleBlur} onChange={handleChange} />
-                        </div>
-                        <ErrorMessage name="password" className="error" component="span" />
-
-                        <div>
-                            <input type="text" placeholder="Matrícula" name="matricula" className="form-control mt-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" onBlur={handleBlur} onChange={handleChange} />
-                        </div>
-                        <ErrorMessage name="matricula" className="error" component="span" />
-
-                        {/* <SelectInput name="papel" label={undefined}>
-                            {optionsRoles.map((option) => (
-                                <option key={option.value} value={option.value}>
-                                    {option.label}
-                                </option>
-                            ))}
-                        </SelectInput>
-                        <ErrorMessage name="papel" className="error" component="span" />
-                                
-                        <SelectInput name="funcao" label={undefined}>
-                            {optionsFunctions.map((option) => (
-                                <option key={option.value} value={option.value}>
-                                    {option.label}
-                                </option>
-                            ))}
-                        </SelectInput>
-                        <ErrorMessage name="funcao" className="error" component="span" />
- */}
-
-                        <div className="d-grid gap-2 mt-3">
-                            <input type="submit" value="Enviar" className="btn btn-primary" disabled={isSubmitting} />
-                        </div>
-                        <form />
-                    </div>
-                </div>
-            </div>
-        )}
-    </Formik>
-      )
+                    <input type="submit" value="Register" disabled={isSubmitting} onClick={handleRegister} />
+                    
+                </form>
+            )}
+        </Formik>
+    )
 };
 
 export default RegisterPage;
